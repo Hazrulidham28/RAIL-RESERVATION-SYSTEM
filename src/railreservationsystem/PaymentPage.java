532262c981fa6ff09dadcb2f84f93c21a17f0aa2
ALIF SAFWAN <alifsafwanz30@gmail.com>
2023-01-08 14:31:45 +0800

    update design only

--- src/railreservationsystem/PaymentPage.java
+++ src/railreservationsystem/PaymentPage.java
@@ -6,12 +6,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package railreservationsystem;
-import java.io.*;
-import java.util.Scanner;
 /**
  *
  * @author alifs
  */
+  
+
+
+
+
+
+
+
 public class PaymentPage extends javax.swing.JFrame {
 
     /**
@@ -19,8 +25,8 @@ public class PaymentPage extends javax.swing.JFrame {
      */
     public PaymentPage() {
         initComponents();
-    }
-
+        
+    }   
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -32,29 +38,16 @@ public class PaymentPage extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jTextPane2 = new javax.swing.JTextPane();
         jScrollPane3 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jScrollPane5 = new javax.swing.JScrollPane();
-        jTextPane3 = new javax.swing.JTextPane();
-        jScrollPane6 = new javax.swing.JScrollPane();
-        jTextPane4 = new javax.swing.JTextPane();
-        jLabel1 = new javax.swing.JLabel();
-        jScrollPane7 = new javax.swing.JScrollPane();
-        jTextPane5 = new javax.swing.JTextPane();
-        jPanel2 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
-        jPanel4 = new javax.swing.JPanel();
-        jPanel5 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
         jPanel7 = new javax.swing.JPanel();
         label1 = new java.awt.Label();
-        button4 = new java.awt.Button();
-        button5 = new java.awt.Button();
-        label2 = new java.awt.Label();
-        jPanel8 = new javax.swing.JPanel();
+        jScrollPane4 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jPanel1 = new javax.swing.JPanel();
+        button5 = new java.awt.Button();
+        button4 = new java.awt.Button();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
@@ -65,14 +58,7 @@ public class PaymentPage extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTextPane1);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(0, 0, 636, 40);
-
-        jTextPane2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        jTextPane2.setText("Order Summary");
-        jScrollPane2.setViewportView(jTextPane2);
-
-        getContentPane().add(jScrollPane2);
-        jScrollPane2.setBounds(0, 40, 360, 30);
+        jScrollPane1.setBounds(0, 0, 630, 40);
 
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
@@ -81,45 +67,6 @@ public class PaymentPage extends javax.swing.JFrame {
         getContentPane().add(jScrollPane3);
         jScrollPane3.setBounds(0, 30, 640, 0);
 
-        jTextPane3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        jTextPane3.setText("                Fare");
-        jScrollPane5.setViewportView(jTextPane3);
-
-        getContentPane().add(jScrollPane5);
-        jScrollPane5.setBounds(500, 40, 140, 30);
-
-        jTextPane4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        jTextPane4.setText("             Quantity");
-        jScrollPane6.setViewportView(jTextPane4);
-
-        getContentPane().add(jScrollPane6);
-        jScrollPane6.setBounds(350, 40, 150, 30);
-
-        jLabel1.setText("Quantity");
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(410, 50, 45, 16);
-
-        jScrollPane7.setViewportView(jTextPane5);
-
-        getContentPane().add(jScrollPane7);
-        jScrollPane7.setBounds(390, 40, 64, 22);
-
-        jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(jPanel2);
-        jPanel2.setBounds(360, 70, 140, 160);
-
-        jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(jPanel3);
-        jPanel3.setBounds(500, 70, 140, 160);
-
-        jPanel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(jPanel4);
-        jPanel4.setBounds(360, 230, 140, 40);
-
-        jPanel5.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(jPanel5);
-        jPanel5.setBounds(500, 230, 140, 40);
-
         jPanel6.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 2));
         jPanel6.add(jPanel7);
 
@@ -127,7 +74,35 @@ public class PaymentPage extends javax.swing.JFrame {
         jPanel6.add(label1);
 
         getContentPane().add(jPanel6);
-        jPanel6.setBounds(0, 270, 640, 30);
+        jPanel6.setBounds(0, 260, 630, 30);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null},
+                {null, null, null, null, null}
+            },
+            new String [] {
+                "Type", "Origin", "Destination", "Quantity", "Fare"
+            }
+        ));
+        jScrollPane4.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane4);
+        jScrollPane4.setBounds(0, 40, 630, 220);
+
+        jPanel1.setLayout(new java.awt.GridLayout(1, 0));
+
+        button5.setLabel("Online Banking");
+        button5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                button5ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(button5);
 
         button4.setLabel("Credit Card");
         button4.addActionListener(new java.awt.event.ActionListener() {
@@ -135,26 +110,17 @@ public class PaymentPage extends javax.swing.JFrame {
                 button4ActionPerformed(evt);
             }
         });
-        getContentPane().add(button4);
-        button4.setBounds(410, 320, 170, 80);
+        jPanel1.add(button4);
 
-        button5.setLabel("Online Banking");
-        getContentPane().add(button5);
-        button5.setBounds(80, 320, 160, 80);
-
-        label2.setFont(new java.awt.Font("Dialog", 1, 12)); // NOI18N
-        label2.setText("TOTAL");
-        getContentPane().add(label2);
-        label2.setBounds(300, 240, 40, 20);
-
-        jPanel8.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        getContentPane().add(jPanel8);
-        jPanel8.setBounds(0, 230, 360, 40);
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 70, 360, 160);
+        jPanel1.setBounds(0, 290, 630, 140);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void button5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_button5ActionPerformed
 
     private void button4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button4ActionPerformed
         // TODO add your handling code here:
@@ -194,37 +160,25 @@ public class PaymentPage extends javax.swing.JFrame {
             public void run() {
                 new PaymentPage().setVisible(true);
             }
-        });
+        }); 
+    }
         
-      }
-        
-    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private java.awt.Button button4;
     private java.awt.Button button5;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
-    private javax.swing.JPanel jPanel8;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
-    private javax.swing.JScrollPane jScrollPane5;
-    private javax.swing.JScrollPane jScrollPane6;
-    private javax.swing.JScrollPane jScrollPane7;
+    private javax.swing.JScrollPane jScrollPane4;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextPane jTextPane1;
-    private javax.swing.JTextPane jTextPane2;
-    private javax.swing.JTextPane jTextPane3;
-    private javax.swing.JTextPane jTextPane4;
-    private javax.swing.JTextPane jTextPane5;
     private java.awt.Label label1;
-    private java.awt.Label label2;
     // End of variables declaration//GEN-END:variables
 }
+
+
+
