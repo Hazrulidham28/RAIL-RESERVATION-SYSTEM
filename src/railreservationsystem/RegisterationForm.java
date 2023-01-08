1f4a74d47d24224969f20aae3581ca578f5a8d08
Hazrul <2021816444@student.uitm.edu.my>
2023-01-08 18:10:19 +0800

    update

--- src/railreservationsystem/RegisterationForm.java
+++ src/railreservationsystem/RegisterationForm.java
@@ -19,6 +19,7 @@ public class RegisterationForm extends javax.swing.JFrame {
     public String Contact;
     public String Password;
     private boolean passData=true;
+    public String status;
     
     
    
@@ -41,6 +42,7 @@ public class RegisterationForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jTextEmail = new javax.swing.JTextField();
         jTextName = new javax.swing.JTextField();
@@ -56,54 +58,58 @@ public class RegisterationForm extends javax.swing.JFrame {
         jButtonSubmit = new javax.swing.JButton();
         jPassword = new javax.swing.JTextField();
         jButtonClear = new javax.swing.JButton();
+        jRadioStudent = new javax.swing.JRadioButton();
+        jRadioOKU = new javax.swing.JRadioButton();
+        jRadioSenior = new javax.swing.JRadioButton();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setForeground(java.awt.Color.white);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setText("Registeration ");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 10, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 10, -1, -1));
 
         jTextEmail.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextEmailActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 58, 202, -1));
-        getContentPane().add(jTextName, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 155, 202, -1));
+        getContentPane().add(jTextEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 80, 270, 30));
+        getContentPane().add(jTextName, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 200, 270, 30));
 
         jTextIc.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextIcActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextIc, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 211, 202, -1));
+        getContentPane().add(jTextIc, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 260, 270, 30));
 
         jTextGender.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextGenderActionPerformed(evt);
             }
         });
-        getContentPane().add(jTextGender, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 261, 202, -1));
-        getContentPane().add(jTextContact, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 317, 202, -1));
+        getContentPane().add(jTextGender, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 320, 270, 30));
+        getContentPane().add(jTextContact, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 380, 270, 30));
 
         jLabelEmail.setText("Email");
-        getContentPane().add(jLabelEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 36, -1, -1));
+        getContentPane().add(jLabelEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 60, 80, 10));
 
         jLabelPass.setText("Password");
-        getContentPane().add(jLabelPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 86, -1, -1));
+        getContentPane().add(jLabelPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 120, 100, 10));
 
         jLabelName.setText("Name");
-        getContentPane().add(jLabelName, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 136, -1, -1));
+        getContentPane().add(jLabelName, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 180, 90, 10));
 
         jLabelIc.setText("Identity Card Number");
-        getContentPane().add(jLabelIc, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 189, -1, -1));
+        getContentPane().add(jLabelIc, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 240, 170, 10));
 
         jLabelGender.setText("Gender");
-        getContentPane().add(jLabelGender, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 239, -1, -1));
+        getContentPane().add(jLabelGender, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 300, 90, 10));
 
         jLabelContact.setText("Contact Number");
-        getContentPane().add(jLabelContact, new org.netbeans.lib.awtextra.AbsoluteConstraints(137, 295, -1, -1));
+        getContentPane().add(jLabelContact, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 360, 140, 10));
 
         jButtonSubmit.setText("REGISTER");
         jButtonSubmit.addActionListener(new java.awt.event.ActionListener() {
@@ -111,14 +117,14 @@ public class RegisterationForm extends javax.swing.JFrame {
                 jButtonSubmitActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonSubmit, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 370, 109, 39));
+        getContentPane().add(jButtonSubmit, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 630, 130, 40));
 
         jPassword.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jPasswordActionPerformed(evt);
             }
         });
-        getContentPane().add(jPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 110, 200, -1));
+        getContentPane().add(jPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 140, 270, 30));
 
         jButtonClear.setText("CLEAR");
         jButtonClear.addActionListener(new java.awt.event.ActionListener() {
@@ -126,7 +132,32 @@ public class RegisterationForm extends javax.swing.JFrame {
                 jButtonClearActionPerformed(evt);
             }
         });
-        getContentPane().add(jButtonClear, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 370, 110, 40));
+        getContentPane().add(jButtonClear, new org.netbeans.lib.awtextra.AbsoluteConstraints(730, 630, 130, 40));
+
+        buttonGroup1.add(jRadioStudent);
+        jRadioStudent.setText("Student");
+        jRadioStudent.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioStudentActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jRadioStudent, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 450, 120, 30));
+
+        buttonGroup1.add(jRadioOKU);
+        jRadioOKU.setText("OKU");
+        jRadioOKU.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jRadioOKUActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jRadioOKU, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 480, 100, 30));
+
+        buttonGroup1.add(jRadioSenior);
+        jRadioSenior.setText("Senior Citizen");
+        getContentPane().add(jRadioSenior, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 510, 150, 30));
+
+        jLabel2.setText("Status");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 420, 90, 20));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -147,6 +178,15 @@ public class RegisterationForm extends javax.swing.JFrame {
         IcNum = jTextIc.getText();
         Gender = jTextGender.getText();
         Contact = jTextContact.getText();
+        if(jRadioStudent.isSelected()){
+            status="Student";
+        }
+        else if(jRadioOKU.isSelected()){
+            status="OKU";
+        }
+        else{
+            status="Senior Citizen";
+        }
         
         //This if else state is to give prompt message if user not give any value and will return false value
         if(Email.equals("")){
@@ -193,10 +233,11 @@ public class RegisterationForm extends javax.swing.JFrame {
                 outFile.print(Name+";");
                 outFile.print(IcNum+";");
                 outFile.print(Gender+";");
-                outFile.println(Contact+";");
+                outFile.print(Contact+";");
+                outFile.println(status+";");
                 //outFile.println();
                 
-                
+                //testvv
                 
                 outFile.close();
                 
@@ -217,7 +258,7 @@ public class RegisterationForm extends javax.swing.JFrame {
         login.setVisible(true);
         this.dispose();
         
-        
+        //
         
     }//GEN-LAST:event_jButtonSubmitActionPerformed
 
@@ -239,6 +280,14 @@ public class RegisterationForm extends javax.swing.JFrame {
         jTextContact.setText("");
         JOptionPane.showMessageDialog(null,"Successfully cleared!");
     }//GEN-LAST:event_jButtonClearActionPerformed
+
+    private void jRadioStudentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioStudentActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jRadioStudentActionPerformed
+
+    private void jRadioOKUActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioOKUActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jRadioOKUActionPerformed
 
     
     
@@ -279,9 +328,11 @@ public class RegisterationForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JButton jButtonClear;
     private javax.swing.JButton jButtonSubmit;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabelContact;
     private javax.swing.JLabel jLabelEmail;
     private javax.swing.JLabel jLabelGender;
@@ -289,6 +340,9 @@ public class RegisterationForm extends javax.swing.JFrame {
     private javax.swing.JLabel jLabelName;
     private javax.swing.JLabel jLabelPass;
     private javax.swing.JTextField jPassword;
+    private javax.swing.JRadioButton jRadioOKU;
+    private javax.swing.JRadioButton jRadioSenior;
+    private javax.swing.JRadioButton jRadioStudent;
     private javax.swing.JTextField jTextContact;
     private javax.swing.JTextField jTextEmail;
     private javax.swing.JTextField jTextGender;
